aa8432b389cbf86e9bf211e847ff6c59bb43f23f
fluxworks <mrjustinlucas@gmail.com>
2025-09-09 18:48:43 -0400

    25.9.9.1845
    Added symbols file support

--- makeexe.bat
+++ makeexe.bat
@@ -1,7 +1,7 @@
 @echo off
 del f.exe flat.exe pe64demo.exe
 echo flat x64
-fasm.exe stage0.asm f.exe
-f.exe stage1.asm flat.exe
+fasm.exe stage0.asm f.exe -s f.fas
+f.exe stage1.asm flat.exe -s flat.fas
 flat.exe pe64demo.asm pe64demo.exe
 pe64demo.exe
