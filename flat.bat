25c54fa34ee2ac3971cee5539b3313e138a91356
fluxworks <mrjustinlucas@gmail.com>
2026-06-17 19:25:25 -0400

    Add files via upload

---
+++ flat.bat
@@ -0,0 +1,11 @@
+del out\flat.exe out\f0.exe out\f1.exe out\f2.exe out\x64.exe
+out\f.exe src\flat.asm out\flat.exe
+out\flat.exe src\flat.asm out\f0.exe
+out\f0.exe src\flat.asm out\f1.exe
+out\f1.exe src\flat.asm out\f2.exe
+out\flat.exe src\x64.asm out\x64.exe
+out\x64.exe
+del out\flat.exe out\f0.exe out\f1.exe out\f2.exe out\x64.exe
+
+
+
